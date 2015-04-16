2ca6e959a2d756e465c5ecef8d8dd1e5c952ab36
Chris Toshok <toshok@dropbox>
2015-04-16 08:23:23 -0700

    ANF-ize this

--- tests/sieve1.js
+++ tests/sieve1.js
@@ -1,21 +1,33 @@
 function primes (n) {
-  function primes_internal (cur, remaining, filter) {
-    if (remaining === 0)
-      return;
-    else {
-      if (!filter(cur)) {
-	print (cur);
-	primes_internal (cur+1, remaining-1, function prime_filter (test) {
-			   return test%cur === 0 || filter (test);
-			 });
-      }
-      else {
-      	primes_internal (cur+1, remaining, filter);
-      }
+    function primes_internal (cur, remaining, filter) {
+	let u5 = print("testing: ");
+	let u6 = print(cur);
+	let u7 = print(remaining);
+	if (remaining === 0)
+	    return;
+	else {
+	    let t = filter(cur);
+	    let nt = !t;
+	    if (nt) {
+		let u1 = print (cur);
+		let prime_filter = function prime_filter (test) {
+		    let mod = test%cur;
+		    if (mod === 0) { let u4 = print("mod === 0"); return true; }
+		    let ft = filter (test);
+		    if (ft) { let u5 = print("upchain filter returned true"); return true; }
+		    return false;
+		};
+		primes_internal (cur+1, remaining-1, prime_filter);
+	    }
+	    else {
+		let u3 = print ("about to call prime_internals 2");
+		
+      		primes_internal (cur+1, remaining, filter);
+	    }
+	}
     }
-  }
-
-  primes_internal (2, n, function base_filter (test) { return false; });
+    
+    primes_internal (2, n, function base_filter (test) { return false; });
 }
 
 primes (100);
